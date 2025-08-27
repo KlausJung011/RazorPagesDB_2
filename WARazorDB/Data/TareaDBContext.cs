16c6cdf0670a787a177ae301df99c8d3fdce4a65
KlausJung011 <nicolas.nsavs@gmail.com>
2025-08-26 21:07:03 -0400

    Paquetes Nugget y conexión a  SQL Server

---
+++ WARazorDB/Data/TareaDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WARazorDB.Models;
+
+namespace WARazorDB.Data
+{
+    public class TareaDBContext : DbContext
+    {
+        public TareaDBContext(DbContextOptions<TareaDBContext> options) : base(options) //Tarea investigar
+        {
+        }
+
+        public DbSet<Tarea> Tareas { get; set; }  //Investigar
+
+        protected TareaDBContext()
+        {
+
+        }
+    }
+}
